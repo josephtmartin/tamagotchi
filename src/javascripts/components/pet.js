5d0ff6005a9f0ec5e903effde13e8b0e7cb4135b
josephtmartin <goldglovejo13@gmail.com>
2020-09-06 16:52:46 -0500

    changed font weight

--- src/javascripts/components/pet.js
+++ src/javascripts/components/pet.js
@@ -1,7 +1,7 @@
 const pet = () => {
   const domString = `
     <div class="pet-container">
-      <h1>Coughy Boi</h1>
+      <h1>Coughy Cat</h1>
       <img class="pet-image" src="https://data.whicdn.com/images/337309690/original.png?t=1573586811" alt="Coughing Cat Image">
     </div>
   `;
